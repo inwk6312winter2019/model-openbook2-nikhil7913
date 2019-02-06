2fa27892c8ddd030d2ac19294155eb91ae67c992
nikhil <Nikhil.Nikhil@dal.ca>
2019-02-06 13:17:05 -0800

    first

---
+++ 1.py
@@ -0,0 +1,9 @@
+def tuple_required(filename):
+   fin = open(filename,'r')
+   for line in fin:
+     line=line.strip()
+     tp=(line[2],line[3],line[6],line[7])
+     print(tp)
+
+fin = open('Street_Centrelines.csv','r')
+print(tuple_required('Street_Centrelines.csv'))
